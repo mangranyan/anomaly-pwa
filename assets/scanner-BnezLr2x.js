2396346eed853f96cf452269f69b24010c886a6e
mangranyan <3041622669@qq.com>
2025-12-26 12:42:33 +0800

    Add files via upload

---
+++ assets/scanner-BnezLr2x.js
@@ -0,0 +1,3 @@
+let n=null;const r=async c=>{const e=document.getElementById("scanner-container"),s=document.getElementById("qr-video"),i=document.getElementById("close-scanner");e.classList.remove("hidden"),n=new ZXing.BrowserQRCodeReader,i.onclick=()=>{n.reset(),e.classList.add("hidden")};try{await n.decodeFromVideoDevice(void 0,s,(d,o)=>{d&&(n.reset(),e.classList.add("hidden"),c(d.getText()))})}catch{alert(`请允许摄像头权限以扫描二维码
+
+【重试】`),e.classList.add("hidden")}},t=document.createElement("script");t.src="https://cdn.jsdelivr.net/npm/@zxing/library@0.18.3/umd/index.min.js";t.onload=()=>{window.ZXing=ZXing};document.head.appendChild(t);export{r as startScan};
